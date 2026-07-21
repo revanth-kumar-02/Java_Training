7a6740d43069d8551513b75ade8af39b2b62bb1c
Revanth <revanthp0201@gmail.com>
2026-07-21 14:59:43 +0530

    Day-1 Training of Java

---
+++ day1/greeting.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class greeting {
+    public static void main(String[] args) {
+        System.out.println("revanth");
+    }
+}
---
+++ day1/kyc.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class kyc {
+    public static void main(String[] args) {
+        
+        String name="Revanth" , pan = "YAKY2950", address = "Namakkal, Tiruchengode",atm = "SBI";
+        long aadhar = 6326371404L, mobile = 9787872051L;
+        System.out.println("Thanks for showing interest in our "+atm+" The User "+name+" has been registered");
+    }
+}
