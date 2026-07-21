f3382e82087c8050517194203eea145db1d1a78c
Revanth <revanthp0201@gmail.com>
2026-07-21 16:23:55 +0530

    income calculator by if/else condition

---
+++ day1/income.java
@@ -0,0 +1,32 @@
+package day1;
+import java.util.Scanner;
+
+public class income {
+    public static void main(String[] args) {
+        Scanner income = new Scanner(System.in);
+
+        double salary = 0, tax = 0;
+        System.out.println("Income calculator");
+        System.out.println("Enter the Annual Salary ");
+        salary = income.nextDouble();
+        if(salary>=30.1) {
+            tax = (salary*30)/100;
+        }
+        else if(salary>15.0&&salary<=20.0) {
+            tax = (salary*10)/100;
+        }
+        else if(salary>20.1&&salary<=25.0) {
+            tax = (salary*15)/100;
+        }
+        else if(salary>25.1&&salary<=30.0) {
+            tax = (salary*25)/100;
+        }
+        else if(salary>12.0&&salary<=15.0) {
+            tax = (salary*5)/100;
+        }
+        else{
+            tax = 0;
+        }
+        System.out.println(salary+" earnings need to pay "+tax);
+    }
+}
