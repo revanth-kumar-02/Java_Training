cb839cea86fdd86036c98478e7239229e1612039
Revanth <revanthp0201@gmail.com>
2026-07-22 07:49:00 +0530

    Day 1 homework

---
+++ day1/currencyconverter.java
@@ -0,0 +1,41 @@
+package day1;
+import java.util.Scanner;
+public class currencyconverter {
+    public static void main(String[] args) {
+        Scanner money = new Scanner(System.in);
+        int option;
+        double usd = 0.01, eur = 0.009, amount = 0, convertedValue = 0;
+        double inrUsd = 96.0, inrEur = 110.0;
+
+        System.out.println("Currency Converter");
+        System.out.println("Choose which currency :");
+        System.out.println("1. INR to USD");
+        System.out.println("2.USD to INR");
+        System.out.println("3.INR to EUR");
+
+        System.out.println("Enter the currency: ");
+        option = money.nextInt();
+
+        if(option == 1) {
+            System.out.println("INR To USD Converter ");
+            System.out.println("Enter the amount: ");
+            amount = money.nextDouble();
+            convertedValue = amount*usd;
+            System.out.println("The given amount is "+amount+" and the converted value is "+convertedValue);
+
+        }
+        if(option == 2) {
+            System.out.println("INR To EUR Converter ");
+            System.out.println("Enter the amount: ");
+            amount = money.nextDouble();
+            convertedValue = amount*eur;
+            System.out.println("The given amount is "+amount+" and the converted value is "+convertedValue);
+
+        }
+
+
+
+
+
+    }
+}
