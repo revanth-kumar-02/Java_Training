18855cf4d222642d7fd6d9734d44a5ea13ffe693
Revanth <revanthp0201@gmail.com>
2026-07-23 14:36:44 +0530

    Max and min program

---
+++ day3/max_and_min.java
@@ -0,0 +1,23 @@
+package day3;
+
+import java.util.Scanner;
+
+public class max_and_min {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+
+        System.out.println("Enter num1 and num2:");
+        int num1 = number.nextInt();
+        int num2 = number.nextInt();
+
+        int diff = num1 - num2;
+        int buffer = (diff >> 31) & 1;
+        int max = num1 - diff * buffer;
+        int min = num2 + diff * buffer;
+
+        System.out.println("Max " + max);
+        System.out.println("Min " + min);
+
+        number.close();
+    }
+}
