242588b7f8d73010415694544ded75e83e29e372
Revanth <revanthp0201@gmail.com>
2026-07-23 12:17:58 +0530

    finding odd or even using bitwise operator

---
+++ day3/bitmanipulation.java
@@ -0,0 +1,19 @@
+package day3;
+
+import java.util.Scanner;
+
+public class bitmanipulation {
+    public static void main(String[] args) {
+        Scanner check = new Scanner(System.in);
+        int num = 0;
+        System.out.println("Enter the number ");
+        num = check.nextInt();
+
+        if((num&1)==0) System.out.println("Even");
+        else System.out.println("Odd");
+        //positive or negative
+        int result = ~num+1;
+        System.out.println(result);
+        check.close();
+    }
+}
